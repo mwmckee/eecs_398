c5cbd0d73487c687b2c878a3ec7e50927318da80
Casey <css77@case.edu>
2014-03-01 16:31:34 -0500

    added manage rider menu

---
+++ app_code/Bicycle Bluetooth Diagnostics/gen/com/bbdt/bluetoothbicyclediagnostics/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.bbdt.bluetoothbicyclediagnostics;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
