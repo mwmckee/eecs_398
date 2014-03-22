d4472290ccf8ae792a223114378960ce9e285181
Casey <css77@case.edu>
2014-03-22 15:35:43 -0400

    started dialogue boxes
    Signed-off-by: Casey <css77@case.edu>

---
+++ app_code/Bicycle Bluetooth Diagnostics/src/com/bbdt/bluetoothbicyclediagnostics/dialogs/create_account_dialog.java
@@ -0,0 +1,13 @@
+package com.bbdt.bluetoothbicyclediagnostics.dialogs;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+public class create_account_dialog extends DialogFragment {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		
+	}
+}
